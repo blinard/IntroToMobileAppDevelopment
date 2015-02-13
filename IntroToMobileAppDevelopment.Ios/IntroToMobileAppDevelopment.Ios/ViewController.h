0c4bd69b6d6d29e206ea98d50ee2d27b70731b61
Brad Linard <brad.linard@idelata.com>
2015-02-13 07:54:54 -0500

    Finishing up iOS example.

--- IntroToMobileAppDevelopment.Ios/IntroToMobileAppDevelopment.Ios/ViewController.h
+++ IntroToMobileAppDevelopment.Ios/IntroToMobileAppDevelopment.Ios/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *btnGetANumber;
+@property (weak, nonatomic) IBOutlet UILabel *lblNumber;
 
 @end
 
--- IntroToMobileAppDevelopment.Ios/IntroToMobileAppDevelopment.Ios/ViewController.m
+++ IntroToMobileAppDevelopment.Ios/IntroToMobileAppDevelopment.Ios/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <UNIRest.h>
 
 @interface ViewController ()
 
@@ -14,9 +15,33 @@
 
 @implementation ViewController
 
+@synthesize btnGetANumber;
+@synthesize lblNumber;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [btnGetANumber addTarget:self action:@selector(btnGetANumber_onTouchUpInside:) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+- (void)btnGetANumber_onTouchUpInside:(id)sender {
+    NSDictionary *headers = @{@"accept": @"application/json"};
+    
+    [[UNIRest get:^(UNISimpleRequest *request) {
+        [request setUrl:@"http://introtomobileappdevelopment.azurewebsites.net/api/RandomNumber"];
+        [request setHeaders:headers];
+    }] asStringAsync:^(UNIHTTPStringResponse *stringResponse, NSError *error) {
+        NSString *result = stringResponse.body;
+        [self performSelectorOnMainThread:@selector(updateLabel:) withObject:[NSString stringWithFormat: @"Your number is\n%@",result] waitUntilDone:true];
+    }];
+    
+}
+
+- (void)updateLabel:(NSString*) value {
+    [lblNumber setText:value];
+    
 }
 
 - (void)didReceiveMemoryWarning {
